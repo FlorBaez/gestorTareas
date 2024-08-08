8895aee7d04fe40c9d5676eb0d31fa8adb12c83c
Flor Baez <fpozosbaez@gmail.com>
2024-08-08 16:39:54 -0600

    Compilación con webpack, no añade tareas

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,20 @@
 const path = require('path');
 
 module.exports = {
+    mode:'development', // Para saber si esta en desarrollo o en producción 
     entry: './src/index.js', // Punto de entrada de tu aplicación
     output: {
         filename: 'bundle.js', // Nombre del archivo de salida
         path: path.resolve(__dirname, 'dist'), // Carpeta de salida
     },
-    module:{
+    module: {
         rules: [
             {
                test: /\.css$/, // Regex para identificar archivos CSS
                use: ['style-loader', 'css-loader'], //Loaders para procesar archivos CSS
             },
             {
-               tes: /\.js$/, // Regex para identificararchivos JS
+               test: /\.js$/, // Regex para identificararchivos JS
                exclude: /node_modules/, //Excluir la carpeta node_modules 
                use: {
                    loader: 'babel-loader', // Loader para convertir JS moderno al JS compatible a más navegadores
@@ -26,7 +27,9 @@ module.exports = {
     },
     devtool: 'source-map', // Genera source maps para facilitar la depuración 
     devServer: {
-        contentBase: path.resolve(__dirname, 'dist'), // Carpeta del que correra el servidor
+        static: {
+            directory: path.resolve(__dirname, 'dist'), // Carpeta del que correra el servidor
+        },
         compress: true, // Hbilitar compresión gzip
         port: 9000, // Puerto delservidor de desarrollo 
     },
